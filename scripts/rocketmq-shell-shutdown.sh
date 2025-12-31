46d3304fea6c9bdf8ea1bcb2573651ab7486e291
zjxjwxk <zjxjwxk@gmail.com>
2025-12-31 19:53:15 +0800

    Add RocketMQ shutdown script

---
+++ scripts/rocketmq-shell-shutdown.sh
@@ -0,0 +1,3 @@
+sh /Library/rocketmq-all-4.8.0-source-release/distribution/target/rocketmq-4.8.0/rocketmq-4.8.0/bin/mqshutdown broker
+
+sh /Library/rocketmq-all-4.8.0-source-release/distribution/target/rocketmq-4.8.0/rocketmq-4.8.0/bin/mqshutdown namesrv
