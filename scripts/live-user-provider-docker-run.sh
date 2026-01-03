d2493403ac4f8e854ba1550352b9afa7dbfb796d
zjxjwxk <zjxjwxk@gmail.com>
2026-01-03 22:02:48 +0800

    Remove .idea from Git tracking

--- scripts/live-user-provider-docker-run.sh
+++ scripts/live-user-provider-docker-run.sh
@@ -1,5 +1,5 @@
-docker run --name live-user-provider-latest \
- -p 192.168.31.73:9090:9090 \
- -e DUBBO_IP_TO_REGISTRY=192.168.31.73 \
- --add-host 'live.zjxjwxk.com:192.168.31.73' \
+docker run --name live-user-provider \
+ -p "$HOST_IP":9090:9090 \
+ -e DUBBO_IP_TO_REGISTRY="$HOST_IP" \
+ --add-host "live.zjxjwxk.com:$HOST_IP" \
  -d live-user-provider-docker:latest
